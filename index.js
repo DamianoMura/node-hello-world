c1e15b3fe74fcdb8e9dedd170a71f18a9c9d2a72
DamianoMura <mura.damiano14@gmail.com>
2025-07-30 13:27:28 +0200

    [add] passing parameters

--- index.js
+++ index.js
@@ -2,6 +2,15 @@
 //sapendo che process.argv.length senza parametri passati dal terminale ha valore 2 perchè contiene in posizione 0 la path di node e in posizione 1 la path del file che stiamo aprendo. possiamo determinare che se process.argv.length > 2 allora ho passato dei parametri quindi:
 if(process.argv.length>2){
   console.log("hai passato dei parametri")
+  //in questo caso facciamo finta che dobbiamo passare del testo e vogliamo dare l'opportunità di non usare gli apici =)
+  //dichiaro una stringa dove inserisco tutti i parametri ... 
+  let data = process.argv[2];
+  // li ciclo e concateno formando un unica frase
+  for (let x = 3 ; x < process.argv.length ; x++ ){
+    data+=" "+process.argv[x];
+  }
+  //stampo con console.log
+  console.log(data)
 }
 else{
   console.log("hello world");
